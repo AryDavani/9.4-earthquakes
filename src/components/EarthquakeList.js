88818c35215d3ff29877283c1869140f679ccfca
Ary Davani <arydavani@gmail.com>
2017-07-20 15:13:58 -0400

    working

--- src/components/EarthquakeList.js
+++ src/components/EarthquakeList.js
@@ -10,10 +10,10 @@ export default class EarthquakeList extends Component {
         <div className="col-sm-6" key={result.id}>
           <div className="card">
             <div className="card-block">
-              <h4 className="card-title">{result.place}</h4>
-              <h6 className="card-subtitle mb-2 text-muted">Magnitude: {result.mag}</h6>
+              <h4 className="card-title">{result.properties.place}</h4>
+              <h6 className="card-subtitle mb-2 text-muted">Magnitude: {result.properties.mag}</h6>
               <h6 className="card-subtitle mb-2 text-muted">Time: {moment(result.time).format('llll')}</h6>
-              <p className="card-text">Coordinates: {result.coordinates}</p>
+              <p className="card-text">Coordinates: {result.geometry.coordinates}</p>
 
               <a href={result.url} className="card-link">USGS Event Link</a>
 
